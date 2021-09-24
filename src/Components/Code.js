0e4f589d1585c79af94a9aad05d9f8151eeeb459
commonPlaceC <aalex.03@mail.ru>
2021-09-24 15:46:27 +0300

    update

--- src/Components/Code.js
+++ src/Components/Code.js
@@ -9,7 +9,7 @@ export default class Code extends Component {
                 <p className="codep unselectable">You can contact me if you have quastions<br/>
                     about programming thigs<br/>
                     Now i use: Python3,<br/>
-                    Learning: JS, HTML, CSS.
+                    Learning: JS, HTML, CSS, ReactJs/ReactNative.
                 </p>
             </div>
         )
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,10 +4,9 @@ export default class Form extends Component {
     render() {
         return (
             <p>As know as CommonPlace<br/>
-                I'm a student. I like programming and photos<br/>
+                I'm a student of RTU MIREA. I like web and mobile developing and photos<br/>
                 And it's my official page. Now it's developing and testing, so 
-                many functions are not available.
-                We will fix it soon!
+                many functions are not available or don't work correctly. We will fix it soon!
             </p>
         );
     }
